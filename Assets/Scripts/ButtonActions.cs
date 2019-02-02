a2a5e7cd78bdbe536e308bcfffd1dd208d5dcc58
Disruptive Clarity <paul@disruptiveclarity.com>
2019-02-02 12:59:34 +1100

    Initial commit

---
+++ Assets/Scripts/ButtonActions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActions : MonoBehaviour
+{
+    public void OpenTwitter()
+    {
+        Application.OpenURL("https://twitter.com/paularmistead");
+    }
+
+    public void OpenInstagram()
+    {
+        Application.OpenURL("https://www.instagram.com/disruptiveclarity/");
+    }
+
+    public void OpenLinkedIn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/paularmistead");
+    }
+
+    public void OpenEmail()
+    {
+        Application.OpenURL("mailto:paularmistead@disruptiveclarity.com");
+    }
+}
---
+++ Assets/VideoPlaylist.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoPlaylist : MonoBehaviour
+{
+
+    public RawImage image;
+    public List<VideoClip> videoClipList;
+    private List<VideoPlayer> videoPlayerList;
+    private int videoIndex;
+
+    void Start()
+    {
+        StartCoroutine(PlayVideo());
+    }
+
+    IEnumerator PlayVideo(bool firstRun = true)
+    {
+        if(firstRun)
+        {
+            videoPlayerList = new List<VideoPlayer>();
+            for (int i = 0; i < videoClipList.Count; i++)
+            {
+                //Create new Object to hold the Video and the sound then make it a child of this object
+                GameObject vidHolder = new GameObject("VP" + i);
+                vidHolder.transform.SetParent(transform);
+
+                //Add VideoPlayer to the GameObject
+                VideoPlayer videoPlayer = vidHolder.AddComponent<VideoPlayer>();
+                videoPlayerList.Add(videoPlayer);
+
+                //Add AudioSource to  the GameObject
+                AudioSource audioSource = vidHolder.AddComponent<AudioSource>();
+
+                //Disable Play on Awake for both Video and Audio
+                videoPlayer.playOnAwake = false;
+                audioSource.playOnAwake = false;
+
+                //We want to play from video clip not from url
+                videoPlayer.source = VideoSource.VideoClip;
+
+                //Set Audio Output to AudioSource
+                videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+
+                //Assign the Audio from Video to AudioSource to be played
+                videoPlayer.EnableAudioTrack(0, true);
+                videoPlayer.SetTargetAudioSource(0, audioSource);
+
+                //Set video Clip To Play 
+                videoPlayer.clip = videoClipList[i];
+            }
+        }
+        if (videoIndex >= videoPlayerList.Count)
+            yield break;
+
+        //Prepare video
+        videoPlayerList[videoIndex].Prepare();
+
+        //Wait until this video is prepared
+        while (!videoPlayerList[videoIndex].isPrepared)
+        {
+            Debug.Log("Preparing Index: " + videoIndex);
+            yield return null;
+        }
+        Debug.LogWarning("Done Preparing current Video Index: " + videoIndex);
+
+        //Assign the Texture from Video to RawImage to be displayed
+        image.texture = videoPlayerList[videoIndex].texture;
+
+        //Play first video
+        videoPlayerList[videoIndex].Play();
+        Debug.Log("Playing time: " + videoPlayerList[videoIndex].clip.length);
+        //Wait while the current video is playing
+        bool reachedHalfWay = false;
+        int nextIndex = (videoIndex + 1);
+        while (videoPlayerList[videoIndex].isPlaying)
+        {
+            //Debug.Log("Playing time: " + videoPlayerList[videoIndex].time + " INDEX: " + videoIndex);
+
+            //(Check if we have reached half way)
+            if (!reachedHalfWay && videoPlayerList[videoIndex].time >= (videoPlayerList[videoIndex].clip.length / 2))
+            {
+                reachedHalfWay = true; //Set to true so that we don't evaluate this again
+
+                //Make sure that the NEXT VideoPlayer index is valid. Othereise Exit since this is the end
+                if (nextIndex >= videoPlayerList.Count)
+                {
+                    Debug.LogWarning("End of All Videos: " + videoIndex);
+                    nextIndex = 0;
+                }
+
+                //Prepare the NEXT video
+                Debug.LogWarning("Ready to Prepare NEXT Video Index: " + nextIndex);
+                videoPlayerList[nextIndex].Prepare();
+            }
+            yield return null;
+        }
+        Debug.Log("Done Playing current Video Index: " + videoIndex);
+
+        //Wait until NEXT video is prepared
+        while (!videoPlayerList[nextIndex].isPrepared)
+        {
+            Debug.Log("Preparing NEXT Video Index: " + nextIndex);
+            yield return null;
+        }
+
+        Debug.LogWarning("Done Preparing NEXT Video Index: " + videoIndex);
+
+        //Increment Video index
+        if (videoIndex >= videoPlayerList.Count - 1)
+        {
+            videoIndex = 0;
+        }
+        else
+        {
+            videoIndex++;
+        }
+        Debug.Log("Index: " + videoIndex);
+        //Play next prepared video. Pass false to it so that some codes are not executed at-all
+        StartCoroutine(PlayVideo(false));
+    }
+}
